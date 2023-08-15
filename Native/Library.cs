32ed87788f39f382b9b601a6b1b2cb3b68ade0ef
Yannick <berliner321@googlemail.com>
2023-08-15 09:46:00 +0200

    Update Docs

--- Native/Library.cs
+++ Native/Library.cs
@@ -3,39 +3,19 @@ using System.Runtime.InteropServices;
 
 namespace Yannick.Native
 {
+    /// <summary>
+    /// Represents a utility for loading native libraries and retrieving function pointers.
+    /// </summary>
+    /// <remarks>
+    /// This class provides cross-platform support for loading native libraries on Windows, Linux, and macOS. 
+    /// It also allows retrieving function pointers from the loaded libraries and converting them to delegates.
+    /// </remarks>
     public sealed class Library : IDisposable
     {
         private static Delegate Close;
         private static Delegate Load;
         private static Delegate MethodFind;
         private readonly IntPtr Handle;
-
-        private static class Windows
-        {
-            private const string Library = "kernel32.dll";
-        
-            [DllImport(Library, CharSet = CharSet.Auto)]
-            public static extern IntPtr LoadLibrary(string filename);
-            
-            [DllImport(Library, SetLastError = true)]
-            public static extern void FreeLibrary(IntPtr module);
-
-            [DllImport(Library, CharSet = CharSet.Auto)]
-            public static extern IntPtr GetProcAddress(IntPtr hModule, string procname);
-        }
-        private static class Linux
-        {
-            private const string Library = "libdl.so";
-                
-            [DllImport(Library, CharSet = CharSet.Auto)]
-            public static extern IntPtr dlopen(string filename, int flags);
-            
-            [DllImport(Library, CharSet = CharSet.Auto)]
-            public static extern IntPtr dlsym(IntPtr handle, string symbol);
-
-            [DllImport(Library)]
-            public static extern int dlclose(IntPtr handle);
-        }
 
         static Library()
         {
@@ -53,14 +33,16 @@ namespace Yannick.Native
             }
         }
 
-        private Library(){}
-        
+        private Library()
+        {
+        }
+
         /// <summary>
-        /// Erstellt eine neue Instanz der <see cref="Library"/> Klasse und lädt die angegebene native Bibliothek.
+        /// Creates a new instance of the <see cref="Library"/> class and loads the specified native library.
         /// </summary>
-        /// <param name="path">Der Pfad zur nativen Bibliotheksdatei. Wenn der Pfad nicht absolut ist, wird versucht, die Datei im Systemverzeichnis oder in standardmäßigen Bibliotheksverzeichnissen zu finden.</param>
-        /// <exception cref="FileNotFoundException">Wird geworfen, wenn die angegebene Bibliotheksdatei nicht gefunden wurde.</exception>
-        /// <exception cref="InvalidOperationException">Wird geworfen, wenn die Bibliothek nicht geladen werden kann.</exception>
+        /// <param name="path">The path to the native library file. If the path is not absolute, it will try to find the file in the system directory or in default library directories.</param>
+        /// <exception cref="FileNotFoundException">Thrown if the specified library file is not found.</exception>.
+        /// <exception cref="InvalidOperationException">Will be thrown if the library cannot be loaded.</exception>
         public Library(string path)
         {
             if (!File.Exists(path))
@@ -81,65 +63,80 @@ namespace Yannick.Native
 
             if (!File.Exists(path))
                 throw new FileNotFoundException("Die angegebene Bibliotheksdatei wurde nicht gefunden.", path);
-            
+
             Handle = (IntPtr)(Load.DynamicInvoke(path) ?? throw new InvalidOperationException());
         }
 
         /// <summary>
-        /// Ruft den angegebenen Funktionszeiger aus der nativen Bibliothek ab und wandelt ihn in einen Delegaten um.
+        /// Retrieves the specified function pointer from the native library and converts it to a delegate.
         /// </summary>
-        /// <param name="name">Der Name der Funktion in der nativen Bibliothek.</param>
-        /// <param name="method">Der Delegatentyp, der die Signatur der Funktion repräsentiert.</param>
-        /// <returns>Ein Delegat, das die Funktion repräsentiert, oder null, wenn die Funktion nicht gefunden wurde.</returns>
+        /// <param name="name">The name of the function in the native library.</param>
+        /// <param name="method">The delegate type representing the function's signature.</param>.
+        /// <returns>A delegate representing the function, or null if the function was not found.</returns>.
         public Delegate? this[string name, Delegate method] => _Link(name, method.GetType());
-        
+
+        /// <summary>
+        /// Disposes the native library, releasing the resources associated with it.
+        /// </summary>
+        public void Dispose()
+        {
+            Close.DynamicInvoke(Handle);
+        }
+
+        /// <summary>
+        /// Retrieves the specified function pointer from the native library and converts it to a delegate of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the delegate representing the function's signature.</typeparam>
+        /// <param name="name">The name of the function in the native library.</param>
+        /// <param name="type">Represent the delegate type</param>
+        /// <returns>A delegate of the specified type representing the function, or null if the function was not found.</returns>.
         private Delegate? _Link(string name, Type type)
         {
             var pointer = (IntPtr)MethodFind.DynamicInvoke(Handle, name)!;
             return pointer == IntPtr.Zero ? null : Marshal.GetDelegateForFunctionPointer(pointer, type);
         }
-        
-        /// <summary>
-        /// Ruft den angegebenen Funktionszeiger aus der nativen Bibliothek ab und wandelt ihn in einen Delegaten vom angegebenen Typ um.
-        /// </summary>
-        /// <typeparam name="T">Der Typ des Delegaten, der die Signatur der Funktion repräsentiert.</typeparam>
-        /// <param name="name">Der Name der Funktion in der nativen Bibliothek.</param>
-        /// <returns>Ein Delegat vom angegebenen Typ, das die Funktion repräsentiert, oder null, wenn die Funktion nicht gefunden wurde.</returns>
-        public T? Link<T>(string name) where T : Delegate
-            => (T?) _Link(name, typeof(T));
 
-        public void Dispose()
+        /// <summary>
+        /// Retrieves the specified function pointer from the native library and converts it to a delegate of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the delegate representing the function's signature.</typeparam>
+        /// <param name="name">The name of the function in the native library.</param>
+        /// <returns>A delegate of the specified type representing the function, or null if the function was not found.</returns>.
+        public T? Link<T>(string name) where T : Delegate
+            => (T?)_Link(name, typeof(T));
+
+        /// <summary>
+        /// Provides native methods for working with libraries on Windows.
+        /// </summary>
+        private static class Windows
         {
-            Close.DynamicInvoke(Handle);
+            private const string Library = "kernel32.dll";
+
+            [DllImport(Library, CharSet = CharSet.Auto)]
+            public static extern IntPtr LoadLibrary(string filename);
+
+            [DllImport(Library, SetLastError = true)]
+            public static extern void FreeLibrary(IntPtr module);
+
+            [DllImport(Library, CharSet = CharSet.Auto)]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string procname);
+        }
+
+        /// <summary>
+        /// Provides native methods for working with libraries on Linux and macOS.
+        /// </summary>
+        private static class Linux
+        {
+            private const string Library = "libdl.so";
+
+            [DllImport(Library, CharSet = CharSet.Auto)]
+            public static extern IntPtr dlopen(string filename, int flags);
+
+            [DllImport(Library, CharSet = CharSet.Auto)]
+            public static extern IntPtr dlsym(IntPtr handle, string symbol);
+
+            [DllImport(Library)]
+            public static extern int dlclose(IntPtr handle);
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
